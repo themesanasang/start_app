692e5a45c5a36bee880a21bcc67f29bd52cbe7aa
chakrit sanasang <chakrit.cpe@gmail.com>
2018-12-11 21:38:26 +0700

    add new project starter app flutter

---
+++ README.md
@@ -0,0 +1,8 @@
+# start_app
+
+starter app flutter
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppGameHome'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {},
+          color: Colors.blueAccent,
+          child: Text('Click555'),
+        ),
+      ),
+    );
+  }
+}
